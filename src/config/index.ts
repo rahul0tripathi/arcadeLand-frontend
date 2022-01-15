f2cc5769de05ebf07a3b43185b65ba32987b8ea8
rahultripathi <rahutripathi@ewar.in>
2022-01-16 00:19:44 +0530

    config and utilzzz

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+const TileSetImagePath = "tileset/tileimages"
+const NFT_LAYER = "showcase"
+const ASSET_LAYER = "assets"
+export {TileSetImagePath, NFT_LAYER, ASSET_LAYER}
---
+++ src/util/index.ts
@@ -0,0 +1,114 @@
+import * as phaser from "phaser";
+
+declare interface TileSetConfig {
+    columns: number
+    resolveAddress: string
+    imageheight: number
+    imagewidth: number
+    margin: number
+    name: string
+    spacing: number
+    tilecount: number
+    tileheight: number
+    tilewidth: number
+}
+
+declare interface ModuleConfig {
+    Key: string,
+    Config: TileSetConfig[]
+}
+
+declare interface Root {
+    compressionlevel: number
+    height: number
+    infinite: boolean
+    layers: Layer[]
+    nextlayerid: number
+    nextobjectid: number
+    orientation: string
+    renderorder: string
+    tiledversion: string
+    tileheight: number
+    tilesets: Tileset[]
+    tilewidth: number
+    type: string
+    version: string
+    width: number
+}
+
+declare interface Layer {
+    data: number[]
+    height: number
+    id: number
+    name: string
+    opacity: number
+    type: string
+    visible: boolean
+    width: number
+    x: number
+    y: number
+}
+
+declare interface Tileset {
+    columns: number
+    firstgid: number
+    image: string
+    imageheight: number
+    imagewidth: number
+    margin: number
+    name: string
+    spacing: number
+    tilecount: number
+    tileheight: number
+    tiles?: Tile[]
+    tilewidth: number
+    transparentcolor: string
+}
+
+declare interface Tile {
+    id: number
+    properties: Property[]
+}
+
+declare interface Property {
+    name: string
+    type: string
+    value: boolean
+}
+
+const ResolveAddressPath = function (url: string, key: string, id: integer): string {
+    return `${url}/${key}${id}.png`
+}
+const ResolveName = function (key: string, id: integer): string {
+    return `${key}${id}`
+}
+const LoadTileSetToScene = function (tileset: TileSetConfig[], scene: phaser.Scene): void {
+    for (let set of tileset) {
+        scene.load.image(`${set.name}set`, set.resolveAddress)
+    }
+
+}
+const RandomInRange = function (min: integer, max: integer): integer {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+enum RenderLayerErrors {
+    InvalidPosition,
+    ImageOutOfBounds,
+    LocationAlreadyOccupied
+}
+
+export {
+    ResolveAddressPath,
+    ResolveName,
+    LoadTileSetToScene,
+    RandomInRange,
+    TileSetConfig,
+    ModuleConfig,
+    Tile,
+    Tileset,
+    Root,
+    Property,
+    Layer,
+    RenderLayerErrors
+}
